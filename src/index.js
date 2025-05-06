30875be8a7e2553725ef61b1f6ad4c61b920033e
Sajjad Raza <sajjadraza10p@gmail.com>
2025-05-06 02:00:54 +0200

    add utils for response, error, asyncHandler

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import dotenv from 'dotenv'
 import connectDB from './db/index.js'
+import express from 'express'
+
+const app = express()
 
 dotenv.config({
     path: './env'
 })
 
 connectDB()
+//need to handle error block
+.then(()=>{
+    app.listen(process.env.PORT || 4000), () => {
+        console.log(`Server is running on port: ${process.env.PORT}`)
+    }
+})
+.catch((err)=>{
+    console.log('Mongo DB connection failed!!', err)
+})
